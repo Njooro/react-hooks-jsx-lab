685132ff15bab46f176f45f7deef781d50ba5de8
Maxwell <njoromax5@gmail.com>
2023-11-13 11:55:17 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { image } from "../data/data";
+import {render, screen } from "@testing-library/react"
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p> </p>
+    <img src={ image } alt="I Made This"></img>
+  </div>;
 }
 
 export default About;
